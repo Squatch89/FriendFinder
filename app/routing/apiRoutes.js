419f27f1f0b5653205163bb0171f05f0acae932e
Gene Williams <GeneWillb@Gmail.com>
2017-11-05 23:10:30 -0500

    home page and server page loading

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,7 @@
+const express = require('express');
+const path = require('path');
+const app = module.exports = express();
+
 app.get("/api/friends", function(req, res) {
     //will display all friends
     return res.json(friends);
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,10 +1,10 @@
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
+const express = require('express');
+const path = require('path');
+const app = module.exports = express();
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
 });
-
-app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "survey.html"));
+app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
 });
-
-
-
