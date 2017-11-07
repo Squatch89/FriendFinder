e9839d1ffb8eea83f93f44066f9b1e53f9a55805
Gene Williams <GeneWillb@Gmail.com>
2017-11-07 10:45:55 -0500

    no more [] and modal progress

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const apiRoutes = require('./app/routing/apiRoutes.js');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(htmlRoutes);
 app.use(apiRoutes);
