21f59718a63a15bf9465642702c52c04e8526bd2
Gene Williams <GeneWillb@Gmail.com>
2017-11-06 22:25:28 -0500

    stuck

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,12 +1,12 @@
 const friends = [
     {
         name: "Gene",
-        photo: "link to photo",
+        photo: "https://i.cbc.ca/1.2636404.1399574029!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_620/dna.jpg",
         scores: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
     },
     {
         name: "Alex",
-        photo: "link to photo",
+        photo: "https://news.skyskor.com/wp-content/uploads/2017/10/alex.jpg",
         scores: [5, 4, 3, 2, 1, 5, 4, 3, 2, 1]
     }
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,6 +10,14 @@ app.get("/api/friends", function(req, res) {
 
 app.post("/api/friends", function(req, res) {
     //will handle results from the survey
+    
+    //compare the newFriend against all other friends
+    //determine the lowest "difference" between newFriend and other friends
+    //display the best match
+    const newFriend = req.body;
+    console.log(newFriend);
+    
+    friends.push(newFriend);
     return res.json(friends);
 });
 
